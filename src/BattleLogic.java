6388d6f04f9e535a55543c10ea27ef9a4d6afa22
esima <e.simatov@ya.ru>
2022-09-04 19:42:12 +0400

    seaMaker shot

--- src/BattleLogic.java
+++ src/BattleLogic.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BattleLogic {
 
@@ -14,7 +15,9 @@ public class BattleLogic {
     }
 
     public String[][] shot() {
-        System.out.print("Enter X and Y coord in format (X,Y): ");
+        System.out.println("Enter the shot coords:");
+        Scanner in = new Scanner(System.in);
+        String guess = in.nextLine();
 
     }
 }
@@ -23,9 +26,11 @@ class BattleLogicTestDrive {
     public static void main(String[] args) {
         BattleLogic battleLogic = new BattleLogic();
         String[][] sea = battleLogic.createSea();
+        String[] alphabet = new String[] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "G"};
+        int k = 0;
         System.out.println(" X | 1 2 3 4 5 6 7 8 9 10");
         for (int i=0; i<10; i++) {
-            System.out.print(" Y | ");
+            System.out.print(" " + alphabet[k++] + " | ");
             for (int j=0; j < 10; j++) {
                 System.out.print(sea[i][j] + " ");;
             }
